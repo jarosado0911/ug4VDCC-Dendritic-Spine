9ae68b594e5aa2b1859ba81bd23035ada152fbce
James Rosado <jarosado0911@gmail.com>
2021-03-15 17:28:34 -0400

    added step 5

---
+++ Step5_Run_Calcium_onFullCell_withER/test_run.sh
@@ -0,0 +1,1 @@
+ugshell -ex full_cell_wER.lua -grid simpleGeometry_noReal_SA.ugx -numRefs 0 -tstep 5.0e-6 -endTime 0.030 -setting ryrserca -ryrDensity 3.75 -solver GS -outName output -vtk -pstep 0.0001 
